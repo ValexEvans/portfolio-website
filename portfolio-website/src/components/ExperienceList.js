5acc34dd6290158adf3d5e2ed93f1764ee397071
Valex <Valexevans0@gmail.com>
2025-02-09 22:15:59 -0500

    updated github links

--- portfolio-website/src/components/ExperienceList.js
+++ portfolio-website/src/components/ExperienceList.js
@@ -70,19 +70,19 @@ export default function ExperienceList() {
             {
                 title: "Mobile Application Development for Turntable Project",
                 duration: "02/24 - 04/24",
-                icon: <a href="https://github.com/kilkennylj/Turntable" target="_blank" rel="noopener noreferrer"><FaGithub /></a>,
+                icon: <a href="https://github.com/ValexEvans/turntable-app" target="_blank" rel="noopener noreferrer"><FaGithub /></a>,
                 summary: "Developed API endpoints for seamless mobile app-backend connectivity. Coordinated with the front-end team to integrate and design the landing mobile wireframe.",
             },
             {
                 title: "Frontend Development for LAMP Stack Contact Manager Project",
                 duration: "01/24 - 02/24",
-                icon: <a href="https://github.com/Katoromic/LAMPStack" target="_blank" rel="noopener noreferrer"><FaGithub /></a>,
+                icon: <a href="https://github.com/ValexEvans/lamp-stack-project" target="_blank" rel="noopener noreferrer"><FaGithub /></a>,
                 summary: "Focused on the Front-End development, gaining proficiency in PHP, MySQL, and JavaScript.Presented a LAMP stack contact manager project, showcasing professionalism, standards adherence, and clear communication.",
             },
             {
                 title: "Image Classifier Using Convolutional Neural Networks",
                 duration: "10/23 - 12/23",
-                icon: null,
+                icon: <a href="https://github.com/ValexEvans/robot-vision" target="_blank" rel="noopener noreferrer"><FaGithub /></a>,
                 summary: "Developed and trained a Convolutional Neural Network (CNN) using TensorFlow and Keras to classify handwritten digits from the MNIST dataset, achieving a test accuracy of 99%.",
             }
         ]
